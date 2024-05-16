f96ecb09554cbfda192af5f15383ae5108ddf572
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2024-05-16 14:34:19 +0200

    Updated tailwind config media breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,32 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "walking-wake": "url('./src/assets/FeaturedArt/walking-wake-large-up.webp')"
+      },
+      size: {
+        clamp: "clamp(20rem, 22rem,25rem)"
+      },
+      screens: {
+        'xs': { 'max': '370px'},
+        'sm': {'max':'730px'},
+        // => @media (min-width: 640px) { ... }
+  
+        // 'md': {'max':'900px'},
+        'md': '731px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '951px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
