8a7cdf3ae8894d77e4811b4add6f32231250c1f4
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2024-05-16 14:30:58 +0200

    Updated layouts

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,4 +1,3 @@
-import { Sidebar } from '../components/Sidebar'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import twimasq from '../assets/FeaturedArt/twimasq.webp'
@@ -29,41 +28,6 @@ export function HomeLayout() {
                 <h1 className='text-3xl sm:w-full block text-white font-medium sm:text-center mb-2 p-2 bg-teal-600' htmlFor="container">Coming soon </h1>
                 <div className='grid sm:py-4 gap-4 lg:grid-cols-3 xs:flex flex-col items-center sm:px-4 sm:grid-cols-2 md:grid-cols-2 w-full h-fit ' name='container'>
                     <Product products={frontPage}/>
-                    {/* { frontPage.map(item => (
-                    <div key={self.id} className='flex flex-col h-full border-black border-2 md:pt-4'>
-                        <Link to={`products/${item.type}/${item.id}`}>
-                            <img className='mb-4 scale-[65%] hover:scale-[75%] transition-transform duration-200' src={item.image} />
-                            <h3 className='text-center px-4 lg:text-xl sm:text-xl md:text-xl text-wrap'>{item.productName}</h3>
-                        </Link>
-                        { item.stock > 0 ? (
-                            <h3 className='text-green-600 font-medium text-center'>Stock: {item.stock > 36 ? '>36' : item.stock}pcs</h3>
-                         ) : (
-                            <h3 className=' font-medium text-center text-red-500'>Sold out</h3>
-                         )}
-                         <div className='flex justify-end h-full flex-col'>
-                            <div className='flex justify-between h-fit sm:justify-end sm:flex-col mt-4 items-center mx-4'>
-                                <h3 className={`md:text-xl sm:text-2xl mb-4 block ${item.onSale ? 'text-emerald-700' : 'text-black'} border-transparent border-2 font-medium text-yello w-700 p-2 relative`}>
-                                    { item.onSale &&
-                                        <span className='absolute text-base text-red-600 p-1 leading-none top-[-10px] left-[-2px] font-medium line-through decoration-2'>
-                                            {item.originalPrice}
-                                        </span>
-                                    }
-                                    {item.price},- czk
-                                </h3>
-                                { item.stock > 0 ? (
-                                    <button className='relative group mb-4 border-black border-2 p-2'>
-                                        <span className='md:text-xl sm:text-2xl z-10 relative group-hover:text-white transition-colors duration-200 font-medium'>{item.released ? "Buy now" : 'Pre-order'}</span>
-                                        <div className='absolute w-[calc(100%)] h-full bg-teal-600 [transform:rotateY(90deg)] group-hover:[transform:rotateY(0deg)] duration-100 top-0 z-[0] left-[0px]'></div>
-                                    </button>
-                                ) : (
-                                    <button disabled className='relative group bg-gray-300 mb-4 border-black border-2 p-2'>
-                                        <span className='md:text-xl sm:text-2xl z-10 text-black relative font-medium'>Unavailable</span>
-                                    </button>
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                    ))} */}
                 </div>
             </div>
         </main>
--- src/layouts/ProductLayout.jsx
+++ src/layouts/ProductLayout.jsx
@@ -1,14 +1,33 @@
-import { useParams } from "react-router"
-import { Product } from "../components/Product"
+import { useNavigate, useParams } from "react-router"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import ReactPaginate from 'react-paginate'
+import { Sidebar } from "../components/Sidebar"
+import { Product } from "../components/Product"
 import { filter } from "../helpers/filter"
+import { search } from '../helpers/search'
+import { paginate } from "../helpers/paginate"
+import { urlquery } from '../helpers/urlquery'
+import { paginateUrl } from "../helpers/paginateUrl"
+import { urlfragment } from "../helpers/urlfragment"
+import { ResultsHeading } from "../components/resultsHeading"
+import { Timeline } from "../components/Timeline"
 
-export function ProductLayout () {
+export function ProductLayout({ param }) {
 
-    const {categoryId} = useParams()
+    const navitage = useNavigate()
+    const { categoryId } = useParams()
+    const [localParam, setLocalParam] = useState(categoryId)
     const [products, setProducts] = useState([])
     const [filters, setFilters] = useState([])
-    const expansions = ['Scarlet & Violet','Crown Zenith','Silver Tempest', 'Lost Origin','Pokémon GO','Astral Radiance']
+    const [stockCheck, setStockCheck] = useState(false)
+    const [releasedCheck, setReleasedCheck] = useState(false)
+    const [saleCheck, setSaleCheck] = useState(false)
+    const [min, setMin] = useState(50)
+    const [max, setMax] = useState(9950)
+
+    const urlFragment = urlfragment()
+    const page = urlFragment ? urlFragment : 0
 
     useEffect(() => {
         let ignore = false
@@ -17,42 +36,151 @@ export function ProductLayout () {
         if (!ignore) {
             fetch(url)
                 .then(res => res.json())
-                .then(json => setProducts(json.filter(product => product.type === categoryId)))
+                .then(json => setProducts(json))
         }
         return () => { ignore = true }
     }, [categoryId])
 
-    function checkboxHandler (e, val) {
+    // using 2 seperate param states kinda fixes screen
+    // flashing with previous page's render
+    // I'd like to do it another way but I'm very tired right now
+    if (param !== localParam) {
+        setFilters([])
+        setMin(0)
+        setMax(9950)
+        setStockCheck(false)
+        setReleasedCheck(false)
+        setSaleCheck(false)
+        setLocalParam(param)
+    }
+
+
+    function checkBox(e, val) {
         const tempFilters = structuredClone(filters)
+
         if (e.target.checked) {
             tempFilters.push(val)
             setFilters(tempFilters)
-            return
         } else {
             const index = tempFilters.indexOf(val)
-            tempFilters.splice(index,1)
-            console.log("working:"+index)
+            tempFilters.splice(index, 1)
             setFilters(tempFilters)
         }
     }
 
-    const shownProducts = filter(products, filters)
+    function stockCheckHandler(e) {
+        setStockCheck(e.target.checked)
+    }
+
+    function releasedCheckHandler(e) {
+        setReleasedCheck(e.target.checked)
+    }
+
+    function saleCheckHandler(e) {
+        setSaleCheck(e.target.checked)
+    }
+
+    function minHandler(e) {
+        const num = Number(e.target.value)
+        num < max && setMin(num)
+    }
+
+    function maxHandler(e) {
+        const num = Number(e.target.value)
+        num > min && setMax(num)
+    }
+
+    function minInputHandler(e) {
+        const num = Number(e.target.value)
+        num <= max ? setMin(num) : setMin(max)
+    }
+
+    function maxInputHandler(e) {
+        const num = Number(e.target.value)
+        num >= min ? setMax(num) : setMax(min)
+    }
+
+    function paginationNavigationBecausePaginationAnchorsRefuseToHandleInputEvenThoughHrefIsValid(value) {
+        const url = paginateUrl(categoryId, value)
+        console.log(url)
+        navitage(url)
+    }
+
+    let validProducts = products
+    let queryString = ''
+
+    if (categoryId === 'search') {
+        console.log(categoryId)
+        queryString = urlquery()
+
+        const filteredList = products.filter(product =>
+            search(product, queryString)
+        )
+
+        validProducts = filteredList
+    }
+
+    const fragment = urlfragment()
+
+    // memoize the filter calc and send to first page if valid
+    // products change in length
+    validProducts = filter(validProducts, categoryId, filters, stockCheck, releasedCheck, saleCheck, min, max)
+    const pageLength = 6
+    const maxPage = Math.ceil(validProducts.length / pageLength)
+    const shownProducts = paginate(validProducts, pageLength, page)
 
     return (
-        <div className="flex justify-center w-full">
-            <main className="flex max-w-[1420px] gap-4 justify-center sm:flex-col sm:p-4 md:p-12">
-                <div className="lg:w-[45rem] md:w-[30rem] sm:w-full border-black border-2">
-                    {
-                        expansions.map((self,id) => (
-                            <label className="block">
-                                {self}
-                                <input key={self} type="checkbox" onChange={(e) => checkboxHandler(e, self)}></input>
-                            </label>
-                        ))
-                    }
-                </div>
-                <div className="grid lg:grid-cols-3  md:grid-cols-2 sm:grid-cols-2 xs:flex flex-col gap-4">
-                <Product products={shownProducts}/>
+        <div className="flex justify-center h-fit w-full">
+            <main className="flex flex-col max-w-[1420px] flex-1 sm:p-4 md:p-12">
+                <Timeline />
+                <div className="flex max-w-[1420px] w-full h-fit gap-4 justify-center sm:flex-col">
+                    <Sidebar
+                        stockCheck={stockCheck}
+                        releasedCheck={releasedCheck}
+                        saleCheck={saleCheck}
+                        min={min}
+                        max={max}
+                        filters={filters}
+                        checkBox={checkBox}
+                        stockCheckHandler={stockCheckHandler}
+                        releasedCheckHandler={releasedCheckHandler}
+                        saleCheckHandler={saleCheckHandler}
+                        minHandler={minHandler}
+                        maxHandler={maxHandler}
+                        minInputHandler={minInputHandler}
+                        maxInputHandler={maxInputHandler}
+                    />
+                    <div className="w-full flex flex-col sm:mt-4">
+                        <ResultsHeading categoryId={categoryId} queryString={queryString} />
+                        {shownProducts.length ?
+                            <div className="grid w-full lg:grid-cols-3 h-fit md:grid-cols-2 sm:grid-cols-2 xs:flex flex-col gap-4">
+                                <Product products={shownProducts} />
+                            </div>
+                            :
+                            <div className="flex flex-1 justify-center items-center h-full w-full font-medium text-2xl">
+                                <h1>No products found</h1>
+                            </div>
+                        }
+                        <ReactPaginate
+                            breakLabel='...'
+                            nextLabel=''
+                            previousLabel='<'
+                            pageRangeDisplayed={3}
+                            marginPagesDisplayed={1}
+                            pageCount={maxPage > 0 ? maxPage : 0}
+                            onPageChange={(e) => paginationNavigationBecausePaginationAnchorsRefuseToHandleInputEvenThoughHrefIsValid(e.selected + 1)}
+                            renderOnZeroPageCount={undefined}
+                            hrefBuilder={(page) => paginateUrl(categoryId, page)}
+                            className="flex justify-center items-center mt-4 gap-1 "
+                            pageClassName="bg-transparent text-center hover:bg-teal-200 transition-color duration-200 font-medium w-8 h-8 flex justify-center items-center rounded-full"
+                            nextClassName="flex justify-center items-center w-8 h-8"
+                            nextLinkClassName="next-btn"
+                            previousClassName="flex justify-center items-center w-8 h-8"
+                            previousLinkClassName="previous-btn"
+                            disabledLinkClassName="disabled"
+                            forcePage={maxPage > 0 ? fragment-1 : -1}
+                        />
+                    </div>
                 </div>
             </main>
         </div>
