235fc2a896dda839e6c8d55fc484a1e1fd64e749
Bůžek Patrik <165824760+BuzekPDev@users.noreply.github.com>
2024-05-21 16:24:39 +0200

    Updated total helper function

--- src/helpers/total.js
+++ src/helpers/total.js
@@ -2,12 +2,6 @@
 
 export function total (cart) {
 
-    let total = 0
-
-    for (let i in cart) {
-        const price = cart[i].price * cart[i].qty
-        total+=price 
-    }
-
-    return total
+    return cart.reduce((total, item) => 
+        total+Number(item.price)*item.qty, 0)
 }
